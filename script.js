50961faa1e95ce7f3c69e65384208a6f7ee0a459
Unknown <bunce.joe@gmail.com>
2018-12-10 15:23:00 +0000

    Add board rendering

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+function colour(space) {
+    switch (space) {
+        case -1:
+            return 'yellow';
+            break;
+        case 0:
+            return 'white';
+            break;
+        case 1:
+            return 'red';
+            break;
+        default:
+            return false;
+    }
+}
+
 class Board {
     constructor() {
         // Initial board state
@@ -10,6 +26,40 @@ class Board {
                 [1,1,1,1,1,1,1],
                 [-1,-1,1,1,1,-1,-1],
                 [-1,-1,1,1,1,-1,-1]
-            ]
+            ];
+
+        this.container = document.getElementById('container');
+    }
+
+    render() {
+        let table = document.createElement('table');
+        this.board.forEach((row, rowIndex) => {
+            let tr = document.createElement('tr');
+            row.forEach((column, columnIndex) => {
+                let td = document.createElement('td');
+                let space = document.createElement('div');
+                space.className = 'space';
+                space.style.backgroundColor = colour(column);
+                space.onmouseenter = () => {
+
+                };
+                space.onmouseleave = () => {
+
+                };
+                space.onclick = () => {
+
+                };
+                td.appendChild(space);
+                tr.appendChild(td)
+            });
+            table.appendChild(tr);
+        });
+        this.container.innerHTML = '';
+        this.container.appendChild(table)
     }
 }
+
+window.onload = function () {
+    let board = new Board();
+    board.render();
+};
